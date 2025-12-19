f5bc0527d17f7535c4af96bc453e9a76d1a417d1
SophiAoAmaral <167262627+SophiAoAmaral@users.noreply.github.com>
2025-12-18 22:20:09 -0300

    menu-mobile

---
+++ menu-mobile/js/modules/scrollsuave.js
@@ -0,0 +1,21 @@
+export default function initScrollSuave(){
+const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
+
+function scrollToSection(e){
+  e.preventDefault();
+  const href = e.currentTarget.getAttribute('href');
+  const section = document.querySelector(href);
+
+const topo = section.offsetTop;
+  window.scrollTo({
+    top: topo,
+   behavior:'smooth'
+})
+}
+
+
+linksInternos.forEach((link) =>{
+  link.addEventListener('click', scrollToSection);
+})
+}
+ 
---
+++ menu-mobile/js/script.js
@@ -0,0 +1,17 @@
+import initScrollSuave from './modules/scrollsuave.js';
+import initAnimacaoScroll from './modules/scrollanimacao.js'
+import initAccordion from './modules/accordion.js'
+import initTab from './modules/tab-nav.js'
+import initModal from './modules/modal.js'
+import initTootip from './modules/tooltip.js'
+import initDropdownMenu from './modules/initDropdownMenu.js'
+
+
+initScrollSuave();
+initAccordion();
+initAnimacaoScroll();
+initTab();
+initModal();
+initTootip();
+initDropdownMenu();
+
