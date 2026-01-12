902ef9642482f788c4afde1f23d09aad3d9d8553
SophiAoAmaral <167262627+SophiAoAmaral@users.noreply.github.com>
2026-01-12 18:56:11 -0300

    data-objrt

---
+++ date-object/js/script.js
@@ -0,0 +1,20 @@
+import initScrollSuave from './modules/scrollsuave.js';
+import initAnimacaoScroll from './modules/scrollanimacao.js'
+import initAccordion from './modules/accordion.js'
+import initTab from './modules/tab-nav.js'
+import initModal from './modules/modal.js'
+import initTootip from './modules/tooltip.js'
+import initDropdownMenu from './modules/initDropdownMenu.js'
+import menumobile from './modules/menu-mobile.js';
+import animaNumeros from './modules/anima-numeros.js';
+
+initScrollSuave();
+initAccordion();
+initAnimacaoScroll();
+initTab();
+initModal();
+initTootip();
+initDropdownMenu();
+menumobile();
+animaNumeros();
+
